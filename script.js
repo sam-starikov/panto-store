6bfabe41ecb724c8b852b1e2041052075a223574
sam-starikov <sam_starikov@rambler.ru>
2024-06-16 17:40:32 +0400

    add benefit block

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const dotInfoBtn = document.querySelectorAll('.dot-info')
+const tooltips = document.querySelectorAll('.tooltip')
 
 dotInfoBtn.forEach(btn =>
     btn.addEventListener('click', function (e) {
@@ -6,3 +7,18 @@ dotInfoBtn.forEach(btn =>
         console.log(this.parentNode.querySelector('.tooltip').classList.toggle('tooltip--active'))
     })
 )
+
+for (const tooltip of tooltips) {
+    tooltip.addEventListener('click', function stopPropagation(e) {
+        e.stopPropagation()
+    })
+}
+
+document.addEventListener('click', function () {
+    for (const tooltip of tooltips) {
+        tooltip.classList.remove('tooltip--active')
+        tooltip.removeEventListener('click', function stopPropagation(e) {
+            e.stopPropagation()
+        })
+    }
+})
