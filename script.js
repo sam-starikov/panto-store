d4f2cae78f15fc3c1b13bd9d6718ecc1cf35a28d
sam-starikov <sam_starikov@rambler.ru>
2024-06-17 00:53:00 +0400

    add comments for js

--- script.js
+++ script.js
@@ -2,48 +2,43 @@
 const dotInfoBtn = document.querySelectorAll('.dot-info')
 const tooltips = document.querySelectorAll('.tooltip')
 
-//Уст. клик на кнопку открытия tooltip
+//Уст. клик на кнопку для открытия: tooltip
 dotInfoBtn.forEach(btn =>
     btn.addEventListener('click', function (e) {
         e.stopPropagation()
         this.parentNode.querySelector('.tooltip').classList.toggle('tooltip--active')
+
+        //Уст. клик на tooltip и отменяем всплытие
+        for (const tooltip of tooltips) {
+            tooltip.addEventListener('click', stopPropagation)
+        }
     })
 )
 
-// Отменяем всплытие для закрытия tooltip при клике на document
-for (const tooltip of tooltips) {
-    tooltip.addEventListener('click', function stopPropagation(e) {
-        e.stopPropagation()
-    })
-}
-
-//Уст. клик на document для закрытия tooltip и удаляем прослушку
+// Удаляем прослушку с tooltip и закрываем его при клике на document
 document.addEventListener('click', function () {
     for (const tooltip of tooltips) {
         tooltip.classList.remove('tooltip--active')
-        tooltip.removeEventListener('click', function stopPropagation(e) {
-            e.stopPropagation()
-        })
+        tooltip.removeEventListener('click', stopPropagation)
     }
 })
 
+// Отменяем всплытие при закрытии tooltip при клике на document
+const stopPropagation = e => {
+    e.stopPropagation()
+}
+/* end */
+
 /* tabs */
-const tabs = document.querySelectorAll('.tabs__btn')
+const tabsBtn = document.querySelectorAll('.tabs__btn')
 
 // Уст. активный tab
-for (const tab of tabs) {
-    tab.addEventListener('click', function setActive(e) {
-        for (const tab of tabs) {
+for (const tab of tabsBtn) {
+    tab.addEventListener('click', e => {
+        for (const tab of tabsBtn) {
             tab.classList.remove('tabs__btn--active')
         }
-        this.classList.add('tabs__btn--active')
-        tab.closest('.tabs').querySelector('.tabs__btn--active')
+        e.target.classList.add('tabs__btn--active')
     })
 }
-
-// Удаляем прослушку на tabs
-// document.addEventListener('click', function setActive() {
-//     tabs.forEach(tab => {
-//         tab.removeEventListener('click', function setActive() {})
-//     })
-// })
+/* end */
